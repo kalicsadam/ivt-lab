41977f77969a2cdfd46c5bf481fe70b811af5a67
kalicsadam <kalicsadam@gmail.com>
2022-05-06 11:51:41 +0200

    tests

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,15 +10,13 @@ public class GT4500 implements SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new TorpedoStore(10);
-    this.secondaryTorpedoStore = new TorpedoStore(10);
+  public GT4500(TorpedoStore primaryTorpedoStore, TorpedoStore secondaryTorpedoStore) {
+    this.primaryTorpedoStore = primaryTorpedoStore;
+    this.secondaryTorpedoStore = secondaryTorpedoStore;
   }
 
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
-
-    int a = 1;   
 
     return true;
   }
@@ -75,6 +73,7 @@ public class GT4500 implements SpaceShip {
             }
 
             // if both of the stores are empty, nothing can be done, return failure
+            firingSuccess = false; 
           }
         }
         break;
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -6,24 +6,33 @@ import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
+import java.nio.ShortBuffer;
+
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primaryStoreMock;
+  private TorpedoStore secondaryStoreMock;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primaryStoreMock = mock(TorpedoStore.class);
+    secondaryStoreMock = mock(TorpedoStore.class);
+    this.ship = new GT4500(primaryStoreMock, secondaryStoreMock);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primaryStoreMock.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primaryStoreMock, times(1)).fire(1);
   }
 
   @Test
@@ -36,5 +45,90 @@ public class GT4500Test {
     // Assert
     assertEquals(true, result);
   }
+
+  @Test
+  public void check_primary_fired_first() {
+    // Arrange
+    when(primaryStoreMock.fire(1)).thenReturn(true);
+    when(secondaryStoreMock.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+
+    verify(primaryStoreMock, times(1)).fire(1);
+    verify(secondaryStoreMock, times(0)).fire(1);
+  }
+
+  @Test
+  public void check_altering_firing() {
+    ship.fireTorpedo(FiringMode.SINGLE);
+    verify(primaryStoreMock, times(1)).fire(1);
+    verify(secondaryStoreMock, times(0)).fire(1);
+
+    ship.fireTorpedo(FiringMode.SINGLE);
+    verify(primaryStoreMock, times(1)).fire(1);
+    verify(secondaryStoreMock, times(1)).fire(1);
+
+    ship.fireTorpedo(FiringMode.SINGLE);
+    verify(primaryStoreMock, times(2)).fire(1);
+    verify(secondaryStoreMock, times(1)).fire(1);
+
+    ship.fireTorpedo(FiringMode.SINGLE);
+    verify(primaryStoreMock, times(2)).fire(1);
+    verify(secondaryStoreMock, times(2)).fire(1);
+  }
+
+  @Test
+  public void check_if_primary_isempty_fire_secondary() {
+    when(primaryStoreMock.isEmpty()).thenReturn(true);
+
+    ship.fireTorpedo(FiringMode.SINGLE);
+
+    verify(primaryStoreMock, times(0)).fire(1);
+    verify(secondaryStoreMock, times(1)).fire(1);
+  }
+
+ @Test
+ public void check_if_scondary_isempty_fire_primary() {
+   when(secondaryStoreMock.isEmpty()).thenReturn(true);
+
+   ship.fireTorpedo(FiringMode.SINGLE);
+   ship.fireTorpedo(FiringMode.SINGLE);
+
+   verify(primaryStoreMock, times(2)).fire(1);
+   verify(secondaryStoreMock, times(0)).fire(1);
+ }
+
+  @Test
+  public void check_failure_doesnt_fire_other() {
+    when(primaryStoreMock.fire(1)).thenReturn(false);
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    assertEquals(false, result);
+
+    verify(secondaryStoreMock, times(0)).fire(1);
+  }
+
+ @Test
+ public void check_both_empty() {
+  when(primaryStoreMock.isEmpty()).thenReturn(true);
+  when(secondaryStoreMock.isEmpty()).thenReturn(true);
+
+  boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+  assertEquals(false, result);
+ }  
+
+  @Test
+  public void fire_laser() {
+    boolean result = ship.fireLaser(FiringMode.SINGLE);
+    
+    assertEquals(true, result);
+  } 
+
 
 }
